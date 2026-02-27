d3dff45ccde88adb39e32f72edd7f7f0bbf5b3ca
Leon Frölje <lfroelje@uni-bremen.de>
2026-02-27 02:08:48 +0100

    nix aarch64-linux support

--- flake.nix
+++ flake.nix
@@ -22,130 +22,121 @@
       nixvimModules,
     }:
     let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       system = "x86_64-linux";
+      # Helper to iterate over systems
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = nixpkgs.legacyPackages.${system};
+            inherit system;
+          }
+        );
       pkgs = nixpkgs.legacyPackages.${system};
       # python = pkgs.python311;  <-- Change this
-      python = pkgs.python3; # <-- To this (usually 3.12)
-      # --- Custom Packages ---
-
-      models = {
-        alexa = pkgs.fetchurl {
-          url = "https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/alexa_v0.1.onnx";
-          hash = "sha256-b/VmoB0SZw6NnjxZ2jJlHbFXXRcnKmAbf4o5KD37rj4=";
-        };
-        embedding = pkgs.fetchurl {
-          url = "https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/embedding_model.onnx";
-          hash = "sha256-cNFkKQwdCV0dTuFJvF4AVDJQpzFrWfMdBWz/e9MHXB8=";
-        };
-        melspectrogram = pkgs.fetchurl {
-          url = "https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/melspectrogram.onnx";
-          hash = "sha256-uisOD4t7h1NposicsTNg/1O6xDbyiVzO2fR5+mXrF28=";
-        };
-        silero_vad = pkgs.fetchurl {
-          url = "https://github.com/snakers4/silero-vad/raw/v5.1.2/src/silero_vad/data/silero_vad.onnx";
-          hash = "sha256-JiOilT9v89LB5hdAxs23FoEzR5smff7xFKSjzFvdeI8";
-        };
-      };
-      # OpenWakeWord is not in nixpkgs, so we package it here.
-      openwakeword = python.pkgs.buildPythonPackage rec {
-        pname = "openwakeword";
-        version = "0.6.0";
-        pyproject = true;
-
-        src = python.pkgs.fetchPypi {
-          inherit pname version;
-          hash = "sha256-NoWNkPEYPjB0hVl6kSpOPDOEsU6pkj+D/q/658FWVWU=";
-        };
-        postPatch = ''
-          sed -i '/tflite-runtime/d' setup.py
-          if [ -f requirements.txt ]; then
-            sed -i '/tflite-runtime/d' requirements.txt
-          fi
-        '';
-
-        postInstall = ''
-          TARGET_DIR="$out/${python.sitePackages}/openwakeword/resources/models"
-
-          echo "Installing models to: $TARGET_DIR"
-          mkdir -p "$TARGET_DIR"
-
-          # Copy models
-          cp ${models.embedding} "$TARGET_DIR/embedding_model.onnx"
-          cp ${models.melspectrogram} "$TARGET_DIR/melspectrogram.onnx"
-          cp ${models.alexa} "$TARGET_DIR/alexa_v0.1.onnx"
-
-
-          # List files to verify in build logs
-          ls -R "$out/${python.sitePackages}/openwakeword"
-          # Copy assets
-        '';
-        propagatedBuildInputs = with python.pkgs; [
-          onnxruntime
-          scipy
-          scikit-learn
-          numpy
-          setuptools
-          tqdm
-          requests
-        ];
-      };
-
-      # --- Dependency List ---
-      satelliteDependencies = with python.pkgs; [
-        # Core Audio/Video
-        av
-        pyaudio
-        pydub
-        pulsectl
-
-        # AI / Logic
-        openwakeword
-        onnxruntime
-        numpy
-        scipy
-
-        # Network & Storage
-        boto3
-        aiomqtt
-
-        # Utilities
-        pydantic
-        pydantic-settings
-        python-dotenv
-        certifi
-        tqdm
-      ];
 
     in
     {
       # --- PACKAGE BUILD ---
-      packages.${system} = {
-        default = python.pkgs.buildPythonApplication {
-          pname = "voice-satellite";
-          version = "0.1.0";
-          pyproject = true;
-          src = ./.;
+      packages = forAllSystems (
+        { pkgs, system }:
+        let
+          python = pkgs.python3;
 
-          propagatedBuildInputs = satelliteDependencies;
+          models = {
+            alexa = pkgs.fetchurl {
+              url = "https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/alexa_v0.1.onnx";
+              hash = "sha256-b/VmoB0SZw6NnjxZ2jJlHbFXXRcnKmAbf4o5KD37rj4=";
+            };
+            embedding = pkgs.fetchurl {
+              url = "https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/embedding_model.onnx";
+              hash = "sha256-cNFkKQwdCV0dTuFJvF4AVDJQpzFrWfMdBWz/e9MHXB8=";
+            };
+            melspectrogram = pkgs.fetchurl {
+              url = "https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/melspectrogram.onnx";
+              hash = "sha256-uisOD4t7h1NposicsTNg/1O6xDbyiVzO2fR5+mXrF28=";
+            };
+            silero_vad = pkgs.fetchurl {
+              url = "https://github.com/snakers4/silero-vad/raw/v5.1.2/src/silero_vad/data/silero_vad.onnx";
+              hash = "sha256-JiOilT9v89LB5hdAxs23FoEzR5smff7xFKSjzFvdeI8";
+            };
+          };
 
-          # Runtime Dependencies (Binaries)
-          # We need to ensure 'ffmpeg' is in the PATH for pydub to find it
-          nativeBuildInputs = [
-            pkgs.makeWrapper
+          openwakeword = python.pkgs.buildPythonPackage rec {
+            pname = "openwakeword";
+            version = "0.6.0";
+            pyproject = true;
+            src = python.pkgs.fetchPypi {
+              inherit pname version;
+              hash = "sha256-NoWNkPEYPjB0hVl6kSpOPDOEsU6pkj+D/q/658FWVWU=";
+            };
+            postPatch = ''
+              sed -i '/tflite-runtime/d' setup.py
+              if [ -f requirements.txt ]; then
+                sed -i '/tflite-runtime/d' requirements.txt
+              fi
+            '';
+            postInstall = ''
+              TARGET_DIR="$out/${python.sitePackages}/openwakeword/resources/models"
+              mkdir -p "$TARGET_DIR"
+              cp ${models.embedding} "$TARGET_DIR/embedding_model.onnx"
+              cp ${models.melspectrogram} "$TARGET_DIR/melspectrogram.onnx"
+              cp ${models.alexa} "$TARGET_DIR/alexa_v0.1.onnx"
+            '';
+            propagatedBuildInputs = with python.pkgs; [
+              onnxruntime
+              scipy
+              scikit-learn
+              numpy
+              setuptools
+              tqdm
+              requests
+            ];
+          };
+
+          satelliteDependencies = with python.pkgs; [
+            av
+            pyaudio
+            pydub
+            pulsectl
+            openwakeword
+            onnxruntime
+            numpy
+            scipy
+            boto3
+            aiomqtt
+            pydantic
+            pydantic-settings
+            python-dotenv
+            certifi
+            tqdm
           ];
-
-          postInstall = ''
-            mkdir -p $out/${python.sitePackages}/assets
-            mkdir $out/${python.sitePackages}/assets/models
-            cp ${models.silero_vad} $out/${python.sitePackages}/assets/models/silero_vad.onnx
-            if [ -d "./assets/" ]; then
-              cp -r ./assets/* $out/${python.sitePackages}/assets/
-            fi
-            wrapProgram $out/bin/voice-satellite \
-              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.ffmpeg_7-headless ]}
-          '';
-        };
-      };
+        in
+        {
+          default = python.pkgs.buildPythonApplication {
+            pname = "voice-satellite";
+            version = "0.1.0";
+            pyproject = true;
+            src = ./.;
+            propagatedBuildInputs = satelliteDependencies;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            postInstall = ''
+              mkdir -p $out/${python.sitePackages}/assets/models
+              cp ${models.silero_vad} $out/${python.sitePackages}/assets/models/silero_vad.onnx
+              if [ -d "./assets/" ]; then
+                cp -r ./assets/* $out/${python.sitePackages}/assets/
+              fi
+              wrapProgram $out/bin/voice-satellite \
+                --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.ffmpeg_7-headless ]}
+            '';
+          };
+        }
+      );
       nixosModules.default =
         {
           config,
@@ -409,64 +400,46 @@
           };
         };
 
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          name = "Voice satellite dev shell";
-          packages = with pkgs; [
-            (python313.withPackages (pypkgs: with pypkgs; satelliteDependencies))
-          ];
-          shellHook = "zsh";
-        };
-        # (pkgs.buildFHSEnv {
-        #   name = "Python dev shell";
-        #   targetPkgs =
-        #     p: with p; [
-        #       fd
-        #       ripgrep
-        #       (nixvimModules.lib.mkNvim [ nixvimModules.nixosModules.python ])
-        #       # CHANGED: python314 -> python311 (or python312)
-        #       python313
-        #       python313Packages.pip
-        #       python313Packages.virtualenv # Recommended to create a venv inside the FHS
-        #       portaudio
-        #       pkg-config
-        #       zlib
-        #       glib
-        #       # We keep libraries here for runtime, but pip won't need to compile against them
-        #       # if it finds a wheel.
-        #       ffmpeg_7-headless
-        #       cargo
-        #       rustc
-        #       libgcc
-        #       # gccgo15 might be overkill/conflict, standard gcc is usually included in FHS
-        #     ];
-        #   runScript = ''
-        #     zsh
-        #     source .venv/bin/activate
-        #     set -o allexport
-        #     source .env
-        #     set +o allexport
-        #   '';
-        # }).env;
-        uv =
-          (pkgs.buildFHSEnv {
-            name = "uv-shell";
-            targetPkgs =
-              p: with p; [
-                uv
-                zlib
-                glib
-                openssl
-                stdenv.cc.cc.lib
-                (nixvimModules.lib.mkNvim [ nixvimModules.nixosModules.python ])
-              ];
-            runScript = "zsh";
-
-            multiPkgs = p: [
-              p.zlib
-              p.openssl
+      devShells = forAllSystems (
+        { pkgs, system }:
+        let
+          # Reference the dependencies defined in the package scope
+          # We can pull them from self.packages if we want to avoid duplication
+          python = pkgs.python3;
+          # Note: Re-using the logic from above or simply defining shell requirements:
+        in
+        {
+          default = pkgs.mkShell {
+            name = "Voice satellite dev shell";
+            packages = [
+              (pkgs.python313.withPackages (
+                pypkgs:
+                # Re-importing dependency list logic or accessing from packages
+                self.packages.${system}.default.propagatedBuildInputs
+              ))
             ];
-          }).env;
-      };
+            shellHook = "zsh";
+          };
+
+          uv =
+            (pkgs.buildFHSEnv {
+              name = "uv-shell";
+              targetPkgs =
+                p: with p; [
+                  uv
+                  zlib
+                  glib
+                  openssl
+                  stdenv.cc.cc.lib
+                  (nixvimModules.lib.mkNvim [ nixvimModules.nixosModules.python ])
+                ];
+              runScript = "zsh";
+              multiPkgs = p: [
+                p.zlib
+                p.openssl
+              ];
+            }).env;
+        }
+      );
     };
 }
